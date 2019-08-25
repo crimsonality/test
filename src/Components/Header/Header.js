e05f01b1c66912c8279648ccfe42981f5e269d61
Jiwoo Kim <nano10606@gmail.com>
2019-08-24 21:02:47 -0400

    linked in fb swap

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -13,8 +13,8 @@ class Header extends Component {
       <LazyImage src={BackgroundImage} class="img-background"/>
       <div className="social-icon-container">
          <SocialIcon link="https://www.instagram.com/crimsonality/" name="instagram"/>
-         <SocialIcon link="https://www.facebook.com/jiwizzle" name="linkedin"/>
-         <SocialIcon link="https://www.linkedin.com/in/kimjiwoo" name="facebook"/>
+         <SocialIcon link="https://www.facebook.com/jiwizzle" name="facebook"/>
+         <SocialIcon link="https://www.linkedin.com/in/kimjiwoo" name="linkedin"/>
          <SocialIcon link="https://github.com/crimsonality" name="github"/>
       </div>
       <Navbar />
